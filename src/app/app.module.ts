4d21e579e732e520688b4168a3ea6f56c0b1635c
Tushar Balar <balar.tushar1@gmail.com>
2017-07-29 16:19:31 +0530

    Use high chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Deeplinks } from '@ionic-native/deeplinks';
+import { ChartModule } from 'angular2-highcharts';
+import * as highCharts from 'highcharts';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -17,7 +19,8 @@ import { ContentDrawerComponent } from "../components/content-drawer/content-dra
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ChartModule.forRoot(highCharts)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,22 +2,47 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
 export class HomePage {
+
     drawerOptions: any;
 
- 
     constructor(public navCtrl: NavController) {
- 
+
         this.drawerOptions = {
             handleHeight: 50,
             thresholdFromBottom: 50,
             thresholdFromTop: 200,
             bounceBack: true
         };
- 
-  }
+
+    }
+
+  chartOptions = {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Fruit Consumption'
+    },
+    xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit eaten'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+    }, {
+        name: 'John',
+        data: [5, 7, 3]
+    }]
+}
 
 }
